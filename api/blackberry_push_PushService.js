0ce6bcc386714b2b21e6658c5c9498dabff610a0
Michelle Mendoza <mimendoza@rim.com>
2013-01-04 15:54:47 -0500

    fixed indentation
    included learning resources in: app, sensors, system, context menu, covers
    
    Update docs with links to learning resources
    
    Merge all commits

--- api/blackberry_push_PushService.js
+++ api/blackberry_push_PushService.js
@@ -24,12 +24,10 @@
 * <br/><br/>
 * To work with a <code>PushService</code> object, you must first call the static <code>create</code> function.  On a successful create, you will then be able
 * to perform operations using the <code>PushService</code> object you receive in the success callback.
-* <br/><br/>
-* For a great sample app that demonstrates how to use the push APIs, see <a href="https://github.com/blackberry/BB10-WebWorks-Samples/tree/master/pushCapture">Push Capture</a>.
-* <br/><br/>
-* There is also a very valuable developer guide for the Push Capture sample app that you can find <a href='https://developer.blackberry.com/html5/documentation/push_service.html'>here</a>. 
 * @toc {Push} PushService 
 * @class The <code>PushService</code> object allows you to perform all the push-related operations.
+* @learns {Sample - Push Capture} https://github.com/blackberry/BB10-WebWorks-Samples/tree/master/pushCapture Sample that demonstrates how to use the BlackBerry Push API [BlackBerry on GitHub].
+* @learns {Developer Guide} https://developer.blackberry.com/html5/documentation/push_service.html Guide for creating a Push Capture sample app [BlackBerry Developer Documentation].
 * @BB10X
 * @featureID blackberry.push
 * @permission {system} _sys_use_consumer_push Permits your consumer (public) application to use push. This permission is not needed to use enterprise push.
@@ -49,8 +47,8 @@ blackberry.push.PushService = {};
 * (in which case a unique one will be generated for you under the covers) or specifying a unique value of your choosing (this second option is useful if you plan
 * to subscribe with the Push Initiator in your application).  
 * @param {String} [options.ppgUrl] The PPG URL to register with.  If writing a consumer application, you will be provided with this URL after registering to 
-* use the public/BIS PPG push service.  The URL will point to either the eval or the production environment (for eval, https://cp{cpid}.pushapi.eval.blackberry.com; 
-* for production, https://cp{cpid}.pushapi.na.blackberry.com where {cpid} is replaced with your content provider ID).  If writing an enterprise application, 
+* use the public/BIS PPG push service.  The URL will point to either the eval or the production environment (for eval, http://cp{cpid}.pushapi.eval.blackberry.com; 
+* for production, http://cp{cpid}.pushapi.na.blackberry.com where {cpid} is replaced with your content provider ID).  If writing an enterprise application, 
 * no <code>ppgUrl</code> value should be specified.
 * @callback {function} successCallback The callback that is invoked when the <code>create</code> operation is successful.
 * @callback {PushService} successCallback.pushService The <code>PushService</code> object that can be used on a successful <code>create</code> operation.
@@ -73,7 +71,7 @@ blackberry.push.PushService = {};
 * // For a consumer application (using the public/BIS PPG)
 * var ops = { invokeTargetId : 'com.sample.pushtest.target', 
 *             appId : 'appId1', 
-*             ppgUrl : 'https://cp123.pushapi.na.blackberry.com' };
+*             ppgUrl : 'http://cp123.pushapi.na.blackberry.com' };
 *
 * // For an enterprise application (using the enterprise/BES PPG)
 * // var ops = { invokeTargetId : 'com.sample.pushtest.target' }; 
